a70178a45686a95e25343a01655402232bbf6755
Vojaspy <vojasgonnade07@gmail.com>
2024-06-20 01:29:03 +0530

    Skills

--- src/components/Achievements.js
+++ src/components/Achievements.js
@@ -1,9 +1,107 @@
+import LeetCode from "../assets/LeetCode.png";
+import { BoxArrowUpRight } from "react-bootstrap-icons";
+import Hackerrank from "../assets/Hackerrank-new.png";
+import CodeChef from '../assets/Codechef.png';
+import gfg from '../assets/gfg.png';
+
+const sectionStyles = {
+  paddingTop: "125px",
+  paddingLeft: "25px",
+  paddingRight: "25px",
+  backgroundColor: "#e6e6e6",
+};
+
+const headingStyles = {
+  color: "teal",
+};
+
+const liStyles = {
+  fontSize: "20px",
+  margin: "10px 0",
+};
+
 const Achievements = () => {
-    return ( 
-        <section id="achievements">
-            <h1>Achievements</h1>
-        </section>
-     );
-}
- 
+  return (
+    <section id="achievements" style={sectionStyles}>
+      <h1 style={headingStyles}>Achievements</h1>
+      <div className="container-fluid">
+        <div className="row justify-content-center" style={{ marginTop: '30px' }}>
+          <div className="col-md-5 mb-4" style={{ border: '2px solid black', margin: '10px' }}>
+            <a href="https://leetcode.com/u/Vojas_Gonnade/" target="_blank" rel="noopener noreferrer">
+              <img src={LeetCode} alt="LeetCode" className="img-fluid" style={{width:'50%', height:'auto',marginTop:'40px'}}/><span style={{color:'black'}}><BoxArrowUpRight/></span>
+            </a>
+            <ul style={{ marginTop: '30px' }}>
+              <li style={liStyles}>
+                Solved <span style={{ color: 'brown' }}>100+ Problems</span> on LeetCode
+              </li>
+              <li style={liStyles}>
+                Achieved <span style={{ color: 'brown' }}>March LeetCoding Challenge</span> Badge
+              </li>
+              <li style={liStyles}>
+                Achieved <span style={{ color: 'brown' }}>50 Days of LeetCoding</span> Badge
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-5 mb-4" style={{ border: '2px solid black', margin: '10px' }}>
+            <a href="https://www.hackerrank.com/profile/vojasgonnade07" target="_blank" rel="noopener noreferrer">
+              <img src={Hackerrank} alt="Hackerrank" className="img-fluid" style={{width:'50%', height:'auto',marginTop:'40px'}} /><span style={{color:'black'}}><BoxArrowUpRight/></span>
+            </a>
+            <ul style={{ marginTop: '30px' }}>
+              <li style={liStyles}>
+                Achieved <span style={{ color: 'brown' }}>3 star Problem Solving</span> Badge
+              </li>
+              <li style={liStyles}>
+                Achieved <span style={{ color: 'brown' }}>5 star C++</span> Badge
+              </li>
+              <li style={liStyles}>
+                Achieved <span style={{ color: 'brown' }}>5 star Python</span> Badge
+              </li>
+              <li style={liStyles}>
+                Achieved <span style={{ color: 'brown' }}>1 star SQL</span> Badge
+              </li>
+            </ul>
+          </div>
+        </div>
+
+
+        <div className="row justify-content-center" style={{ marginTop: '30px' }}>
+          <div className="col-md-5 mb-4" style={{ border: '2px solid black', margin: '10px' }}>
+            <a href="https://www.codechef.com/users/vojasgonnade" target="_blank" rel="noopener noreferrer">
+              <img src={CodeChef} alt="LeetCode" className="img-fluid" style={{width:'50%', height:'auto',marginTop:'40px'}}/><span style={{color:'black'}}><BoxArrowUpRight/></span>
+            </a>
+            <ul style={{ marginTop: '30px' }}>
+              <li style={liStyles}>
+                <span style={{ color: 'brown' }}>1 star coder</span> on CodeChef
+              </li>
+              <li style={liStyles}>
+                Highest Rating : <span style={{ color: 'brown' }}>1261</span>
+              </li>
+              <li style={liStyles}>
+                Participated in <span style={{ color: 'brown' }}>10+ Contests</span> on CodeChef
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-5 mb-4" style={{ border: '2px solid black', margin: '10px' }}>
+            <a href="https://www.geeksforgeeks.org/user/vojasgonedt2/" target="_blank" rel="noopener noreferrer">
+              <img src={gfg} alt="Hackerrank" className="img-fluid" style={{width:'50%', height:'auto',marginTop:'40px'}} /><span style={{color:'black'}}><BoxArrowUpRight/></span>
+            </a>
+            <ul style={{ marginTop: '30px' }}>
+              <li style={liStyles}>
+                Overall Coding <span style={{ color: 'brown' }}>score 90</span>
+              </li>
+              <li style={liStyles}>
+                Solved <span style={{ color: 'brown' }}>50+ Problems</span> on GFG
+              </li>
+              <li style={liStyles}>
+                Monthly Coding <span style={{ color: 'brown' }}>score 34</span>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default Achievements;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,34 @@
+
+
+const sectionStyles = {
+    paddingTop :'125px',
+    // paddingTop : '15px',
+    paddingLeft : '25px',
+    paddingRight : '25px',
+    backgroundColor : '#e6e6e6',
+    // height :'100vh',
+    // marginBottom : '50px'
+}
+
+
 const Contact = () => {
     return ( 
-        <section id="contact">
+        <section id="contact" style={sectionStyles}>
             <h1>Contact</h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet ultricies sapien. Cras at semper eros. Fusce nec metus augue. Nulla facilisi. Donec id venenatis nisl. Aenean tincidunt varius est, eget efficitur erat efficitur vel. Maecenas non arcu in nisl condimentum lacinia.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet ultricies sapien. Cras at semper eros. Fusce nec metus augue. Nulla facilisi. Donec id venenatis nisl. Aenean tincidunt varius est, eget efficitur erat efficitur vel. Maecenas non arcu in nisl condimentum lacinia.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet ultricies sapien. Cras at semper eros. Fusce nec metus augue. Nulla facilisi. Donec id venenatis nisl. Aenean tincidunt varius est, eget efficitur erat efficitur vel. Maecenas non arcu in nisl condimentum lacinia.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet ultricies sapien. Cras at semper eros. Fusce nec metus augue. Nulla facilisi. Donec id venenatis nisl. Aenean tincidunt varius est, eget efficitur erat efficitur vel. Maecenas non arcu in nisl condimentum lacinia.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet ultricies sapien. Cras at semper eros. Fusce nec metus augue. Nulla facilisi. Donec id venenatis nisl. Aenean tincidunt varius est, eget efficitur erat efficitur vel. Maecenas non arcu in nisl condimentum lacinia.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet ultricies sapien. Cras at semper eros. Fusce nec metus augue. Nulla facilisi. Donec id venenatis nisl. Aenean tincidunt varius est, eget efficitur erat efficitur vel. Maecenas non arcu in nisl condimentum lacinia.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet ultricies sapien. Cras at semper eros. Fusce nec metus augue. Nulla facilisi. Donec id venenatis nisl. Aenean tincidunt varius est, eget efficitur erat efficitur vel. Maecenas non arcu in nisl condimentum lacinia.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet ultricies sapien. Cras at semper eros. Fusce nec metus augue. Nulla facilisi. Donec id venenatis nisl. Aenean tincidunt varius est, eget efficitur erat efficitur vel. Maecenas non arcu in nisl condimentum lacinia.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet ultricies sapien. Cras at semper eros. Fusce nec metus augue. Nulla facilisi. Donec id venenatis nisl. Aenean tincidunt varius est, eget efficitur erat efficitur vel. Maecenas non arcu in nisl condimentum lacinia.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet ultricies sapien. Cras at semper eros. Fusce nec metus augue. Nulla facilisi. Donec id venenatis nisl. Aenean tincidunt varius est, eget efficitur erat efficitur vel. Maecenas non arcu in nisl condimentum lacinia.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet ultricies sapien. Cras at semper eros. Fusce nec metus augue. Nulla facilisi. Donec id venenatis nisl. Aenean tincidunt varius est, eget efficitur erat efficitur vel. Maecenas non arcu in nisl condimentum lacinia.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet ultricies sapien. Cras at semper eros. Fusce nec metus augue. Nulla facilisi. Donec id venenatis nisl. Aenean tincidunt varius est, eget efficitur erat efficitur vel. Maecenas non arcu in nisl condimentum lacinia.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet ultricies sapien. Cras at semper eros. Fusce nec metus augue. Nulla facilisi. Donec id venenatis nisl. Aenean tincidunt varius est, eget efficitur erat efficitur vel. Maecenas non arcu in nisl condimentum lacinia.
+
         </section>
 
      );
